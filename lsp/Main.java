9e07ad73e808214555066b5dceb20ac6f4d02880
Anya Tafliovich <atafliovich@utsc.utoronto.ca>
2020-07-13 14:10:34 -0400

    Fixed the checkstyle issues.

--- lsp/Main.java
+++ lsp/Main.java
@@ -2,22 +2,23 @@ package lsp;
 
 public class Main {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		Rectangle r = getRectangle();
-		r.setHeight(10);
-		r.setWidth(50);
-		System.out.println(r.area()); // it's not a rectangle!
-	}
+  /**
+   * Run a demo.
+   * @param args The ususal.
+   */
+  public static void main(String[] args) {
+    Rectangle r = getRectangle();
+    r.setHeight(10);
+    r.setWidth(50);
+    System.out.println(r.area()); // it's not a rectangle!
+  }
 
-	/*
-	 * The idea here is you get Rectangle objects from 
-	 * somewhere, you don't necessarily know how they 
-	 * are made.
-	 */
-	static Rectangle getRectangle() {
-		return new Square();
-	}
+  /*
+   * The idea here is you get Rectangle objects from 
+   * somewhere, you don't necessarily know how they 
+   * are made.
+   */
+  static Rectangle getRectangle() {
+    return new Square();
+  }
 }
